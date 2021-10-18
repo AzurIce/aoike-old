1ad1c1e0e684785e7c7a0d4d43bfab3d7fba5b10
Azur冰弦 <973562770@qq.com>
2021-10-18 11:04:40 +0800

    Katex

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,8 +1,11 @@
 import MarkdownIt from "markdown-it";
+import MarkdownItKatex from "markdown-it-katex";
 
 const markdownIt = new MarkdownIt({
   html: true,
   breaks: true,
 });
+
+markdownIt.use(MarkdownItKatex);
 
 export default markdownIt;
